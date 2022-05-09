2661c57a896cb26f42d840265ecc9c1fbaadae65
PBx0N <Patsiri.boonprasop@gmail.com>
2022-05-09 20:26:00 +1000

    Final

--- Query.sql
+++
@@ -1,9 +0,0 @@
--- Get data from the imported table --
-SELECT * from HappinessCovid
-
--- Drop ID column --
-ALTER TABLE HappinessCovid DROP COLUMN "ID";
-
--- Check --
-SELECT * from HappinessCovid
-
